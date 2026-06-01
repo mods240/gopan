02dd1d07a1e329c4c0d3977b96f81b8c7c1ff6f3
mods240 <mods240@gmail.com>
2026-06-01 12:18:22 +0900

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/icon-192.png" type="image/png" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
